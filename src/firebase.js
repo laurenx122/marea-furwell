9b4cd25c960ae9867314de8417291e2dd56132f4
Rainelyn Sungahid <rainelynsungahid@gmail.com>
2025-03-11 14:31:22 +0800

    google signin and signup

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'; // Add if using Storage
 // import { getStorage } from 'firebase/storage'; // Add if using Storage
@@ -23,3 +23,4 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
