58493ed62cab783cb6e048b2a958f7cb181ec9b7
Mier <mierangelina47@gmail.com>
2025-03-15 23:32:13 +0800

    FetchingClinics(Admin)

--- src/pages/Admin/AdminHome.js
+++ src/pages/Admin/AdminHome.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
+import { useEffect} from "react";
 import "./AdminHome.css";
 import { Search } from "lucide-react";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 
-//must be replace based on the clinics that already subsribe to the furwell
-const clinics = [
-    { name: "Cebu Veterinary" },
-    { name: "Cebu Veterinary" },
-    { name: "Cebu Veterinary" },
-    { name: "Cebu Veterinary" },
-    { name: "Cebu Veterinary" },
-    { name: "Cebu Veterinary" },
-    { name: "Cebu Veterinary" },
-    { name: "Cebu Veterinary" },
-  ];
-  
-  const AdminHome = () => {
+const AdminHome = () => {
+
+const [clinics, setClinics] = useState([]);
+
+useEffect(() => {
+  const fetchClinics = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "clinics"));
+      const clinicList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setClinics(clinicList);
+    } catch (error) {
+      console.error("Error fetching clinics:", error);
+    }
+  };
+
+  fetchClinics();
+}, []);
     return (
       <div className="admin-home">
         {/* Search Bar */}
@@ -30,7 +40,7 @@ const clinics = [
             <div key={index} className="clinics-card">
               <div className="clinic-image" />
               <p className="clinic-name">Main</p>
-              <p className="clinic-name">Cebu Veterinary</p>
+              <p className="clinic-name">{clinic.clinicName}</p>
             </div>
           ))}
         </div>
