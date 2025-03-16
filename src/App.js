aaf255d4efd75be9da4508fa12087e3e620ed06d
arariadne <elaishaarias@gmail.com>
2025-03-16 17:35:44 +0000

    Main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import Navbar from './components/Navbar/Navbar'; 
+
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import Home from './pages/Home/Home';
@@ -18,44 +21,34 @@ import AdminClinics from './pages/Admin/AdminHome';
 import AdminAnalytics from './pages/Admin/AdminAnalytics';
 import AdminSubscription from './pages/Admin/AdminSubscription';
 import FindClinic from './pages/FindClinic/FindClinic';
-
-const Layout = ({ children }) => {
-  const location = useLocation();
-  const noNavbarRoutes = ['/ClinicHome']; // Add routes where Navbar should not appear
-  //Adding '/PetOwnerHome' soon
-  return (
-    <>
-      {!noNavbarRoutes.includes(location.pathname) && <Navbar />}
-      {children}
-    </>
-  );
-};
+import ClinicDetails from './pages/ClinicDetails/ClinicDetails';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Home" />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/clinicLocator" element={<ClinicLocator />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/maps" element={<Maps />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/appointments" element={<Appointments />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/ClinicSubscribe" element={<ClinicSubscribe />} />
-          <Route path="/ClinicHome" element={<ClinicHome />} />
-          <Route path="/PetOwnerHome" element={<PetOwnerHome />} />
-          <Route path="/VeterinaryHome" element={<VeterinaryHome />} />
-          <Route path="/AdminHome" element={<AdminClinics />} />
-          <Route path="/AdminAnalytics" element={<AdminAnalytics />} />
-          <Route path="/AdminSubscription" element={<AdminSubscription />} />
-          <Route path="/FindClinic" element={<FindClinic />} />
-        </Routes>
-      </Layout>
+      <Navbar /> {/* Navbar at the top */}
+      <Routes>
+      <Route path="/" element={<Navigate to="/Home" />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/clinicLocator" element={<ClinicLocator />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/maps" element={<Maps />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/ClinicSubscribe" element={<ClinicSubscribe />} />
+        <Route path="/ClinicHome" element={<ClinicHome />} />
+        <Route path="/PetOwnerHome" element={<PetOwnerHome />} />
+        <Route path="/VeterinaryHome" element={<VeterinaryHome />} />
+        <Route path="/AdminHome" element={<AdminClinics />} />
+        <Route path="/AdminAnalytics" element={<AdminAnalytics />} />
+        <Route path="/AdminSubscription" element={<AdminSubscription />} />
+        <Route path="/FindClinic" element={<FindClinic />} />
+        <Route path="/FindClinic/:clinicName" element={<ClinicDetails />} />
+        <Route path="/clinic/:clinicId" element={<ClinicDetails />} />
+      </Routes>
     </Router>
   );
 }
